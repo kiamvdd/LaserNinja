97252cf06c274d3b7a9fd2dbf9ab1301809fbc14
kiamvdd <kiamvdd@outlook.com>
2019-05-28 15:49:36 +0200

    Project cleanup

---
+++ Assets/Scripts/UI/FadeOutScrollHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutScrollHelper : MonoBehaviour
+{
+    public FadeOutButtonScrollList FadeOutList;
+
+    public void OnInputDown()
+    {
+        FadeOutList.ReceiveChildInput(transform);
+    }
+}
---
+++ Assets/Scripts/UI/LayoutGroupRefresher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LayoutGroupRefresher : MonoBehaviour
+{
+    RectTransform m_layoutGroup;
+    private void Awake()
+    {
+        m_layoutGroup = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        if (m_layoutGroup == null)
+            return;
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(m_layoutGroup);
+    }
+}
